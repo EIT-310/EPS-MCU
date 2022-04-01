c2a7f44b4da5c3f1c01140c0fb062e08c9fd070b
Nikolaj Juel Andersen <njan20@student.aau.dk>
2022-04-01 16:11:58 +0200

    FSMm2func

--- src/main.cpp
+++ src/main.cpp
@@ -1,136 +1,215 @@
 #include "mbed.h"
+#define BATHIGH 0
+#define BATMIDHIGH 1
+#define BATMID 2
+#define BATMIDLOW 3
+#define BATLOW 4
+#define RADIOPIN D0
+#define OBCPIN D1
+#define ADCSPIN D2
+#define PAYLOADPIN D3
 
+DigitalOut obc(OBCPIN);
+DigitalOut radio(RADIOPIN);
+DigitalOut adcs(ADCSPIN);
+DigitalOut payload(PAYLOADPIN);
 
 // identify state by nr (0,1,2,3)
-typedef struct stateTp {
-  int lastState;
-  int state;
-} stateTp;
+int state;
+int vBat = 6; // TODO: Måske float?
+void getbatHigh(int *stateInfo);
+void getbatMidHigh(int *stateInfo);
+void getbatMid(int *stateInfo);
+void getbatMidLow(int *stateInfo);
+void getbatLow(int *stateInfo);
+
+void batHigh();
+void batMidHigh();
+void batMid();
+void batMidLow();
+void batLow();
+
+void (*getStateFunc[])(int *ss) = {
+  getbatHigh, 
+  getbatMidHigh, 
+  getbatMid, 
+  getbatMidLow, 
+  getbatLow
+  };
+
+void (*stateFunc[])() = {
+  batHigh, 
+  batMidHigh, 
+  batMid, 
+  batMidLow,
+  batLow};
 
 
-void batHigh(stateTp * stateInfo);
-void batMidHigh(stateTp * stateInfo);
-void batMid(stateTp * stateInfo);
-void batMidLow(stateTp * stateInfo);
-void batLow(stateTp * stateInfo);
-
-
-const int BATHIGH = 0;
-
-const int BATMIDHIGH = 1;
-
-const int BATMID = 2;
-
-const int BATMIDLOW = 3;
-
-const int BATLOW = 4;
-int vBat=6; // TODO: Måske float?
-
-void (*stateFunc[])(struct stateTp *ss) = {batHigh, batMidHigh, batMid, batMidLow, batLow};
-//                             state         0     1     2    ...
-
-void batHigh(stateTp * stateInfo)
+void getbatHigh(int *stateInfo)
 {
 
-  printf("\nHIGH\n");
-if (vBat<5)
-{
-      stateInfo->state = BATMIDHIGH;
-}else
-{
-    stateInfo->state = BATHIGH;
-}
+  printf("\ngetHIGH\n");
+  // float = readADC("Sted, Prio");
+  // Læse ADC
+
+  if (vBat < 5)
+  {
+    *stateInfo = BATMIDHIGH;
+  }
+  else
+  {
+    *stateInfo = BATHIGH;
+  }
 
   ThisThread::sleep_for(1s);
   return;
 }
 
-void batMidHigh(stateTp * stateInfo)
+void getbatMidHigh(int *stateInfo)
 {
-  printf("\nMIDHIGH\n");
-    if (vBat>5)
-    {
-        stateInfo->state = BATHIGH;
-    }else if (vBat>4)
-    {   
-        stateInfo->state = BATMIDHIGH;
-    }
-    else
-    {
-        stateInfo->state = BATMID;
-    }
-      ThisThread::sleep_for(1s);
+  printf("\ngetMIDHIGH\n");
+  // Læse ADC
+
+  if (vBat > 5)
+  {
+    *stateInfo = BATHIGH;
+  }
+  else if (vBat > 4)
+  {
+    *stateInfo = BATMIDHIGH;
+  }
+  else
+  {
+    *stateInfo = BATMID;
+  }
+
+  ThisThread::sleep_for(1s);
   return;
 }
 
-void batMid(stateTp * stateInfo)
+void getbatMid(int *stateInfo)
+{
+  printf("\ngetMID\n");
+  // Læse ADC
+  if (vBat > 4)
+  {
+    *stateInfo = BATMIDHIGH;
+  }
+  else if (vBat > 3)
+  {
+    *stateInfo = BATMID;
+  }
+  else
+  {
+    *stateInfo = BATMIDLOW;
+  }
+  ThisThread::sleep_for(1s);
+  return;
+}
+
+void getbatMidLow(int *stateInfo)
+{
+  printf("\ngetMIDLOW\n");
+  // Læse ADC
+  if (vBat > 3)
+  {
+    *stateInfo = BATMID;
+  }
+  else if (vBat > 2)
+  {
+    *stateInfo = BATMIDLOW;
+  }
+  else
+  {
+    *stateInfo = BATLOW;
+  }
+  ThisThread::sleep_for(1s);
+  return;
+}
+
+void getbatLow(int *stateInfo)
+{
+  printf("\ngetBATLOW\n");
+  // Læse ADC
+
+  if (vBat > 2)
+  {
+    *stateInfo = BATMID;
+  }
+  else
+  {
+    *stateInfo = BATLOW;
+  }
+  ThisThread::sleep_for(1s);
+
+  return;
+}
+
+// Udører ting
+
+void batHigh()
+{
+  payload = 1;
+  printf("\nHIGH\n");
+}
+
+void batMidHigh()
+{
+  printf("\nMIDHIGH\n");
+  // Læse ADC
+  obc = 1;
+  radio = 1;
+  payload = 0;
+  adcs = 1;
+  return;
+}
+
+void batMid()
 {
   printf("\nMID\n");
-    if (vBat>4)
-    {
-        stateInfo->state = BATMIDHIGH;
-    }else if (vBat>3)
-    {   
-        stateInfo->state = BATMID;
-    }
-    else
-    {
-        stateInfo->state = BATMIDLOW;
-    }
-    ThisThread::sleep_for(1s);
-    return;
+  obc = 0;
+  radio = 1;
+  payload = 0;
+  adcs = 1;
+  return;
 }
 
-void batMidLow(stateTp * stateInfo)
+void batMidLow()
 {
   printf("\nMIDLOW\n");
-    if (vBat>3)
-    {
-        stateInfo->state = BATMID;
-    }else if (vBat>2)
-    {   
-        stateInfo->state = BATMIDLOW;
-    }
-    else
-    {
-        stateInfo->state = BATLOW;
-    }
-    ThisThread::sleep_for(1s);
-    return;
+    obc = 0;
+    radio = 1;
+    payload = 0;
+    adcs = 1;
+  return;
 }
 
-void batLow(stateTp * stateInfo)
+void batLow()
 {
-  printf("\nledOff\n");
-    if (vBat>2)
-    {
-        stateInfo->state = BATMID;
-    }
-    else
-    {
-        stateInfo->state = BATLOW;
-    }
-    ThisThread::sleep_for(1s);
+  printf("\nLOW\n");
 
-    return;
+  obc = 0;
+  radio = 0;
+  adcs = 0;
+
+  ThisThread::sleep_for(1s);
+
+  return;
 }
 
 int main()
 {
-    stateTp curState;
+  state = BATMID;
 
-    curState.state = BATMID;
-    
-    curState.lastState = BATMID;
-    printf("\nstarting state machine\n");
+  printf("\nstarting state machine\n");
 
-    while (1) {
-        printf("\nback in scheduler");
-        //Læs ADC
-        
-        (*stateFunc[curState.state])(&curState);
+  while (1)
+  {
+    printf("\nback in scheduler");
+    // Læs ADC
 
+    (*getStateFunc[state])(&state);
 
-    }
+    (*stateFunc[state])();
+  }
 }
-
