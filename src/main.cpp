cb83c3b51d2a8bfed784d54e0cb9dd4c8513df85
Nikolaj Juel Andersen <njan20@student.aau.dk>
2022-04-01 09:37:16 +0200

    Virker

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,42 @@
 #include "mbed.h"
 
-// class ISR {
-// public:
-//     ISR(PinName pin) : _interrupt(pin)          // create the InterruptIn on the pin specified to Counter
-//     {
-//         _interrupt.rise(callback(this, &ISR::handle)); // attach increment function of this counter instance
-//     }
-
-//     void handle()
-//     {
-
-//     }
-// private:
-//     InterruptIn _interrupt;
-//     DigitalOut
-// };
-struct ISRPIN
-{
-    InterruptIn chnl;
-    DigitalOut pin;
-
-    ISRPIN(const PinName interruptPin, const PinName GPIOPin) 
-    : chnl(interruptPin), pin(GPIOPin)
-    {}
-    void doInter(){
-        pin=1;
-    }
-}
-isr1(A0,A1);
+class ISR {
+public:
+    ISR(PinName pin, PinName sPin) : _interrupt(pin), swPin(sPin)           // create the InterruptIn on the pin specified to Counter
     
+    {
+        _interrupt.rise(callback(this, &ISR::handle)); // attach increment function of this counter instance
+    }
+
+    void handle()
+    {
+        swPin=1;
+    }
+private:
+    InterruptIn _interrupt;
+    DigitalOut swPin;
+};
+// struct ISRPIN
+// {
+//     InterruptIn chnl;
+//     DigitalOut pin;
+
+//     ISRPIN(const PinName interruptPin, const PinName GPIOPin) 
+//     : chnl(interruptPin), pin(GPIOPin)
+//     {}
+//     chnl.rise(&doInter);
+
+//     void doInter(){
+//         pin=1;
+//     }
+// }
+ISR isr1(A0,A1);
+ISR isr2(A2,A3);    
 
 int main()
 {
     while (1)
     {
-        isr1.chnl.rise(isr1.doInter);
     
             printf("Nothing so far %\n");
         // hal_sleep();
