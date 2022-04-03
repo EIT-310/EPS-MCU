845b80fe0cf17616affa1c4e52445bb00e95f024
Nikolaj Juel Andersen <njan20@student.aau.dk>
2022-04-03 15:37:39 +0200

    virkerikke

--- src/main.cpp
+++ src/main.cpp
@@ -1,215 +1,33 @@
 #include "mbed.h"
-#define BATHIGH 0
-#define BATMIDHIGH 1
-#define BATMID 2
-#define BATMIDLOW 3
-#define BATLOW 4
-#define RADIOPIN D0
-#define OBCPIN D1
-#define ADCSPIN D2
-#define PAYLOADPIN D3
-
-DigitalOut obc(OBCPIN);
-DigitalOut radio(RADIOPIN);
-DigitalOut adcs(ADCSPIN);
-DigitalOut payload(PAYLOADPIN);
-
-// identify state by nr (0,1,2,3)
-int state;
-int vBat = 6; // TODO: Måske float?
-void getbatHigh(int *stateInfo);
-void getbatMidHigh(int *stateInfo);
-void getbatMid(int *stateInfo);
-void getbatMidLow(int *stateInfo);
-void getbatLow(int *stateInfo);
-
-void batHigh();
-void batMidHigh();
-void batMid();
-void batMidLow();
-void batLow();
-
-void (*getStateFunc[])(int *ss) = {
-  getbatHigh, 
-  getbatMidHigh, 
-  getbatMid, 
-  getbatMidLow, 
-  getbatLow
-  };
-
-void (*stateFunc[])() = {
-  batHigh, 
-  batMidHigh, 
-  batMid, 
-  batMidLow,
-  batLow};
+#include "FlashIAPBlockDevice.h"
+#include <TDBStore.h>
 
 
-void getbatHigh(int *stateInfo)
-{
 
-  printf("\ngetHIGH\n");
-  // float = readADC("Sted, Prio");
-  // Læse ADC
+static DigitalOut led(LED1);
 
-  if (vBat < 5)
-  {
-    *stateInfo = BATMIDHIGH;
-  }
-  else
-  {
-    *stateInfo = BATHIGH;
-  }
 
-  ThisThread::sleep_for(1s);
-  return;
-}
+#define FIRST_DB_BLOCK (ADDR_FLASH_SECTOR_6)
 
-void getbatMidHigh(int *stateInfo)
-{
-  printf("\ngetMIDHIGH\n");
-  // Læse ADC
+static FlashIAPBlockDevice full_flash(FIRST_DB_BLOCK, 256 * 1024);
+static TDBStore nvStore(&full_flash);
 
-  if (vBat > 5)
-  {
-    *stateInfo = BATHIGH;
-  }
-  else if (vBat > 4)
-  {
-    *stateInfo = BATMIDHIGH;
-  }
-  else
-  {
-    *stateInfo = BATMID;
-  }
+static uint32_t val;
 
-  ThisThread::sleep_for(1s);
-  return;
-}
+int main() {
+  full_flash.init();
+  int res = nvStore.init();
+  printf("init result = %d\n", res);
 
-void getbatMid(int *stateInfo)
-{
-  printf("\ngetMID\n");
-  // Læse ADC
-  if (vBat > 4)
-  {
-    *stateInfo = BATMIDHIGH;
-  }
-  else if (vBat > 3)
-  {
-    *stateInfo = BATMID;
-  }
-  else
-  {
-    *stateInfo = BATMIDLOW;
-  }
-  ThisThread::sleep_for(1s);
-  return;
-}
+  res = nvStore.get("_0000", &val, sizeof(val));
+  printf("get result = %d, value = %ld\n", res, val);
 
-void getbatMidLow(int *stateInfo)
-{
-  printf("\ngetMIDLOW\n");
-  // Læse ADC
-  if (vBat > 3)
-  {
-    *stateInfo = BATMID;
-  }
-  else if (vBat > 2)
-  {
-    *stateInfo = BATMIDLOW;
-  }
-  else
-  {
-    *stateInfo = BATLOW;
-  }
-  ThisThread::sleep_for(1s);
-  return;
-}
+  val += 1;
+  res = nvStore.set("_0000", &val, sizeof(val), 0);
+  printf("set result = %d\n", res);
 
-void getbatLow(int *stateInfo)
-{
-  printf("\ngetBATLOW\n");
-  // Læse ADC
-
-  if (vBat > 2)
-  {
-    *stateInfo = BATMID;
-  }
-  else
-  {
-    *stateInfo = BATLOW;
-  }
-  ThisThread::sleep_for(1s);
-
-  return;
-}
-
-// Udører ting
-
-void batHigh()
-{
-  payload = 1;
-  printf("\nHIGH\n");
-}
-
-void batMidHigh()
-{
-  printf("\nMIDHIGH\n");
-  // Læse ADC
-  obc = 1;
-  radio = 1;
-  payload = 0;
-  adcs = 1;
-  return;
-}
-
-void batMid()
-{
-  printf("\nMID\n");
-  obc = 0;
-  radio = 1;
-  payload = 0;
-  adcs = 1;
-  return;
-}
-
-void batMidLow()
-{
-  printf("\nMIDLOW\n");
-    obc = 0;
-    radio = 1;
-    payload = 0;
-    adcs = 1;
-  return;
-}
-
-void batLow()
-{
-  printf("\nLOW\n");
-
-  obc = 0;
-  radio = 0;
-  adcs = 0;
-
-  ThisThread::sleep_for(1s);
-
-  return;
-}
-
-int main()
-{
-  state = BATMID;
-
-  printf("\nstarting state machine\n");
-
-  while (1)
-  {
-    printf("\nback in scheduler");
-    // Læs ADC
-
-    (*getStateFunc[state])(&state);
-
-    (*stateFunc[state])();
+  for(;;) {
+    led = !led;
+    wait_us(1000000);
   }
 }
