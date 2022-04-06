03e6f4b60de233f8a520c9be8bac6236d5237650
Albert Laursen <alaur20@student.aau.dk>
2022-04-06 12:12:42 +0200

    read until newline

--- src/main.cc
+++ src/main.cc
@@ -1,73 +1,57 @@
+/*
+ * Copyright (c) 2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "mbed.h"
-#include "FSM.h"
-#include "nv_store.h"
-#include "adc_read.h"
-// Create flash IAP block device
 
-static DigitalInOut led(LED1);
-NVStore nvm;
-AdcRead adc;
-int main() {
-    printf("STATE MACHINE TEST\n");
-    FSM::state current_state;
-    nvm.read(nvm.sm_state,&current_state);
-    FSM sm(FSM::high);
+// Maximum number of element the application buffer can contain
+#define MAXIMUM_BUFFER_SIZE                                                  32
 
+// Create a DigitalOutput object to toggle an LED whenever data is received.
+static DigitalOut led(LED1);
 
+// Create a BufferedSerial object with a default baud rate.
+static BufferedSerial serial_port(USBTX, USBRX);
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "EndlessLoop"
-    while (true) {
+int main(void) {
+    // Set desired properties (9600-8-N-1).
+    serial_port.set_baud(115200);
+    serial_port.set_format(
+            /* bits */ 8,
+            /* parity */ BufferedSerial::None,
+            /* stop bit */ 1
+    );
 
-        nvm.read(nvm.sm_state, &current_state);
-        uint16_t adcVal = adc.read_int(adc.one);
-        printf("Current state: %s\n", sm.to_string().c_str());
-        printf("ADCVal: %d\n",adcVal);
-        if (((float) adcVal / UINT16_MAX)*3.3 > 3.0) {
-            sm.up_state();
-            current_state = sm.getCurrentState();
+    // Application buffer to receive the data
+    char buf[MAXIMUM_BUFFER_SIZE] = {0};
+    char msg[MAXIMUM_BUFFER_SIZE];
+    int cursor;
 
-        } else if (((float) adcVal / UINT16_MAX)*3.3 < 0.8) {
-            sm.down_state();
-            current_state = sm.getCurrentState();
+    while (1) {
+        if (uint32_t num = serial_port.read(buf, sizeof(buf))) {
+            // Toggle the LED.
+            led = !led;
+            for (int i = 0; i < num; ++i) {
+                msg[cursor + i] = buf[i];
+            }
+            cursor += num;
         }
 
-        sm.do_state();
-        nvm.write(nvm.sm_state, &current_state);
-        ThisThread::sleep_for(1s);
+        for (int i = 0; i < cursor; ++i) {
+            if (msg[i] == '\n') {
+                i++;
+                serial_port.write(msg, i);
+                for (int j = 0; j < cursor-1; ++j) {
+                    char tmp = msg[j + i];
+                    msg[j + i] = 0;
+                    msg[j] = tmp;
+                }
+                cursor = cursor - i;
+            }
+        }
     }
-#pragma clang diagnostic pop
-
-
-
-
-
-
-
-
-//    sm.down_state();
-//    current_state = sm.getCurrentState();
-//    printf("Current state: %s\n", sm.to_string().c_str());
-//    sm.do_state();
-//
-
-
-
-
 }
 
-// Hej Abber Labber :)
-//
-//▬▬▬.◙.▬▬▬
-//═▂▄▄▓▄▄▂
-//◢◤ █▀▀████▄▄▄▄◢◤
-//█▄ █ █▄ ███▀▀▀▀▀▀▀╬
-//◥█████◤
-//══╩══╩═
-//╬═╬
-//╬═╬
-//╬═╬
-//╬═╬       Hello?
-//╬═╬  ☻/
-//╬═╬ /▌
-//╬═╬ / \
+
+
