a6d61b56160a67c2d959ddcd75756f66cb5dec56
AAU_PC2 <jlar20@student.aau.dk>
2022-04-08 08:50:55 +0200

    slep

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include "mbed.h"
-#include "platform.h"
 
 int main()
 {
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
     while (1)
     {
-        printf("Jeg Lever\n");
-        ThisThread::sleep_for(500);
-        sleep_manager_unlock_deep_sleep();
-        
+        printf("Going to deeep sleep");
+        lp_ticker_free();
+        hal_deepsleep();
+        lp_ticker_init();
+        printf("Waking up from deeep sleeep");
 
     }
+#pragma clang diagnostic pop
 }
